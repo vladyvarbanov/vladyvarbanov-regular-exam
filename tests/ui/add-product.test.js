7ba925effe0f7a7a84e2878243f8ce97ffd3093c
vladyvarbanov <vladyvarbanov@yahoo.com>
2024-04-14 23:04:30 +0300

    Replacing localhost with 127.0.0.1

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add products page', async ({ page }) => {
-    await page.goto('http://localhost:8081/Add-Product');
+    await page.goto('http://127.0.0.1:8081/Add-Product');
     const form = await page.$('input');
     expect(form).toBeTruthy();
   });
