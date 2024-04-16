495f9ea182e14e9236b95e35a455a2aea9eacebe
vladyvarbanov <vladyvarbanov@yahoo.com>
2024-04-16 23:54:08 +0300

    replace all 8080 with 8888

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('http://localhost:8080/');
+    await page.goto('http://localhost:8888/');
     const heading = await page.$('p');
     const text = await heading.textContent();
     expect(text).toContain('Cookbook');  
