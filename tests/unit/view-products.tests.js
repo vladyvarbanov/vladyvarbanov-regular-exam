1a483ed05a6837cc15dbb9e563d86f8910020ffd
vladyvarbanov <vladyvarbanov@yahoo.com>
2024-04-16 23:59:26 +0300

    replacing all 8888 with 8080

--- tests/unit/view-products.tests.js
+++ tests/unit/view-products.tests.js
@@ -3,13 +3,13 @@ const fetch = require('node-fetch');
 
 suite('View Cookbook page', function() {
   test('Page title', async function() {
-    let res = await fetch("http://localhost:8888/cookbook");
+    let res = await fetch("http://localhost:8080/cookbook");
     let body = await res.text();
     assert.ok(body.includes("<h1>Cookbook</h1>"));
   });
   
   test('My Cookbook', async function() {
-    let res = await fetch("http://localhost:8888/cookbook");
+    let res = await fetch("http://localhost:8080/cookbook");
     let body = await res.text();
     let correctList = body.includes("<ul><li>Water - 1 cup</li><li>Eggs - 3</li><li>Flour - 1 cup</li></ul>");
     assert.ok(correctList, "Cookbook content mismatch");
